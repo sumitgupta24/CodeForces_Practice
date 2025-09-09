3871b8be7cb2bb09bda03ad175a4f1408d4f5978
Sumit Gupta <cricsumit24@gmail.com>
2025-09-09 22:54:04 +0530

    Add new problem solutions for 25_BadBoy and 26_OddDivisor

---
+++ 900ratedproblems/25_BadBoy.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+      int n,m;
+      cin >> n >> m;
+      int i,j;
+      cin >> i >> j;
+      cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+    return 0;
+}
