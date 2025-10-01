abb5909e73d45ad53bf30cbf40d0cc61a214f9ae
Sumit Gupta <cricsumit24@gmail.com>
2025-10-01 15:36:40 +0530

    Add solution for problem 01_EraseFirstorSecondLetter

---
+++ 1100ratedProblems/01_EraseFirstorSecondLetter.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+      long long n;
+      cin >> n;
+      string str;
+      cin >> str;
+      unordered_map<char,int> mpp;
+      for(int i=0;i<n;i++){
+        char ch = str[i];
+        if(mpp.find(ch) == mpp.end()) mpp[ch] = i;
+      }
+      long long total = 0;
+      for(auto& [ch,ind] : mpp){
+        total += (n - ind);
+      }
+      cout << total << endl;
+    }
+    return 0;
+}
